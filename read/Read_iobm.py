b346483fe4e80db505943e39c19d4da2d2a33a85
Hemanta Kumar G <hemant.kumar1704@gmail.com>
2016-04-21 14:54:21 +0530

    Update Read_iobm.py

--- read/Read_iobm.py
+++ read/Read_iobm.py
@@ -9,21 +9,31 @@ import threading
 import datetime, time
 from optparse import OptionParser
 
-N= None # no of sets
-T= None # no of threads
+# No of sample sets to run
+N= None
+# maximum No of threads to run
+T= None
+# output to be stored into the file 
 outfilename= None
-block_size= [0.1, 1, 10, 100, 1000, 5000, 10000]# block size in MB
+# list of data block sizes in MB
+block_size= [0.1, 1, 10, 100, 1000, 5000, 10000]
+# output file handler
+out_write
 
+# thread class which runnes 'dd' command over list of data blocks
 class iobmThread (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID= threadID
         self.name= name
+        
     def run(self):
 	for i in block_size:
+		# active thread count always less than 'T+1' (i.e. main thread+ T), in other words call 'dd' command 
 		if threading.activeCount() <= T+1:
+			# call 'dd' run function over data block[i], with this thread instance
         		iobm_multithread_read(self.name, i)
-			time.sleep(10)
+			#time.sleep(10)
 
 # call free_drivecache() to free drive cache content...		
 #def free_drivecache():
@@ -31,21 +41,29 @@ class iobmThread (threading.Thread):
 #    subprocess.Popen(cmd_free_cache, shell=True)
 
 def iobm_multithread_read(threadName, i):
-    out_write = open(outfilename, 'a') #write operation stdout into file "rx.txt"
-    size= int(i* 1000) # block size in KB
+    # write results (i.e dd command outputs) into file "file_read_thread<i>.txt"
+    #out_write = open(outfilename, 'a')
+    # converting data block size from MB to KB
+    size= int(i* 1000)
+    
+    # if data block size less 1000MB i.e 1GB execute if block, for greater than 1GB datablocks execute else block
     if size <= 1000000:
-#    	free_drivecache()
+    	# clear drive catch
+  	#free_drivecache()
         cmddd= "dd if=/home/hemanta.g/iotest/file"+str(size)+" of=/tmp/file"+str(size) + " bs="+str(size)+"kB count=1"
         p= subprocess.Popen(cmddd, shell=True, stderr=subprocess.PIPE)
         ts_end= datetime.datetime.now()
         wr= str(p.communicate())+threadName+'\n'
     else:
-#	free_drivecache()
+    	# clear drive catch
+	#free_drivecache()
         cmddd= "dd if=/home/hemanta.g/iotest/file1000000 of=/tmp/file1000000 bs=1000000kB count="+str(i/1000)
         p= subprocess.Popen(cmddd, shell=True, stderr=subprocess.PIPE)
         wr= str(p.communicate())+threadName+'\n'
+        
+    # write 'dd' command output into the output file 
     out_write.write(wr)
-    out_write.close()
+    #out_write.close()
     
 def iobm(T):
     thread = None
@@ -53,7 +71,7 @@ def iobm(T):
     for i in range(T):
         thread_instance.append(iobmThread(i, "Thread-"+str(i)))# Create new threads
     for thread in thread_instance: # Start new Threads
-        thread.start()
+	thread.start()
     for thread in thread_instance: # join all new Threads
         thread.join()
 
@@ -62,7 +80,11 @@ def main():
     while i <= N:
 	if threading.activeCount() == 1:
 		print "set count: " +str(i)
-        	iobm(T)# call method to create thread
+		# write results (i.e dd command outputs) into file "file_read_thread<i>.txt"
+		out_write = open(outfilename, 'a')
+        	iobm(T)
+        	out_write.close()
+        	
         	time.sleep(10)
         	i= i+1
     print "Exiting Main Thread"
@@ -71,10 +93,12 @@ if __name__ == "__main__":
     parser= OptionParser()
     parser.add_option("-t", "--threads", dest="threads", default="2", help="total # of threads")
     parser.add_option("-s", "--sampleset", dest="sampleset", default="10", help="total # of sample sets")
+    parser.add_option("-o", "--output", dest="output", default="out_read", help="name of the output file")
     (option, arge)= parser.parse_args()
     
     T= int(option.threads)
     N= int(option.sampleset)
-    outfilename= "file_read_thread" + str(T) + ".txt"
+    O= option.ouput
+    outfilename= O+ str(T) + ".txt"
     
     main()
